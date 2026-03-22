c53dd4b913e2e6419b0ad0ef44616cc7fdcfda37
wasd314 <68897049+wasd314@users.noreply.github.com>
2026-03-22 15:25:43 +0900

    Pollard's rho - Floyd

--- rust/factor/src/pollard_rho.rs
+++ rust/factor/src/pollard_rho.rs
@@ -1,45 +1,79 @@
-use crate::{modint::u128::ModInt, primality::is_prime, utility::gcd};
+use std::marker::PhantomData;
 
-/// Pollard's rho with Floyd's cycle detection.
-pub fn find_factor(n: u128) -> u128 {
-    assert!(n >= 2);
-    if is_prime(n) {
-        return n;
-    }
-    if n.is_multiple_of(2) {
-        return 2;
-    }
-    // n: odd composite
-    let mo = ModInt::new(n);
-    let mut rc = 0;
-    let batch = n.isqrt().isqrt().isqrt();
-    loop {
-        rc += 1;
-        let f = |rx| mo.add(mo.mul(rx, rx), rc);
-        let (mut rx, mut ry) = (rc, rc);
-        let mut d = 1;
-        let mut checkpoint = (rx, ry);
-        while d == 1 {
-            let mut combined = 1;
-            for _ in 0..batch {
-                rx = f(rx);
-                ry = f(f(ry));
-                combined = mo.mul(combined, rx.abs_diff(ry));
-            }
-            d = gcd(n, combined);
-            if d == 1 {
-                checkpoint = (rx, ry);
-            } else if d != n {
-                return d;
-            }
+use crate::{modint::u128::ModInt, utility::gcd, wrapper::Factorize};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Brent {}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Floyd {}
+
+#[derive(Debug, Clone, Copy)]
+pub struct PollardRho<T> {
+    _cd: PhantomData<fn() -> T>,
+    pub batch_gcd: bool,
+}
+
+impl<T> PollardRho<T> {
+    pub fn new(batch_gcd: bool) -> Self {
+        Self {
+            _cd: PhantomData,
+            batch_gcd,
         }
-        (rx, ry) = checkpoint;
-        for _ in 0..batch {
-            rx = f(rx);
-            ry = f(f(ry));
-            let d = gcd(n, rx.abs_diff(ry));
-            if d != 1 && d != n {
-                return d;
+    }
+}
+
+impl Factorize for PollardRho<Floyd> {
+    /// Pollard's rho with Floyd's cycle detection.
+    fn find_factor(&mut self, n: u128) -> u128 {
+        // n: odd composite
+        let mo = ModInt::new(n);
+        let mut rc = 0;
+        loop {
+            rc += 1;
+            let f = |rx| mo.add(mo.mul(rx, rx), rc);
+            if self.batch_gcd {
+                let batch = n.isqrt().isqrt().isqrt();
+                let (mut rx, mut ry) = (rc, rc);
+                let mut d = 1;
+                // combine batch
+                let mut checkpoint = (rx, ry);
+                while d == 1 {
+                    let mut combined = 1;
+                    for _ in 0..batch {
+                        rx = f(rx);
+                        ry = f(f(ry));
+                        combined = mo.mul(combined, rx.abs_diff(ry));
+                    }
+                    d = gcd(n, combined);
+                    if d == 1 {
+                        checkpoint = (rx, ry);
+                    } else if d != n {
+                        return d;
+                    }
+                }
+                // replay batch
+                (rx, ry) = checkpoint;
+                for _ in 0..batch {
+                    rx = f(rx);
+                    ry = f(f(ry));
+                    let d = gcd(n, rx.abs_diff(ry));
+                    if d != 1 && d != n {
+                        return d;
+                    }
+                }
+            } else {
+                // not batch
+                let (mut rx, mut ry) = (rc, rc);
+                while {
+                    rx = f(rx);
+                    ry = f(f(ry));
+                    let d = gcd(n, rx.abs_diff(ry));
+                    if d != 1 && d != n {
+                        return d;
+                    }
+                    d == 1
+                } {}
             }
         }
     }
