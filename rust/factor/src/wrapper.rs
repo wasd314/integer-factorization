7e4030af564135309d25958b0734a605a4df542d
wasd314 <68897049+wasd314@users.noreply.github.com>
2026-03-22 15:23:16 +0900

    trait Factorize

--- rust/factor/src/wrapper.rs
+++ rust/factor/src/wrapper.rs
@@ -1,40 +1,49 @@
 use crate::primality::is_prime;
 
-/// 素因数を 1 つ返す．
-///
-/// `f`: 非自明な約数を返すとする．
-pub fn find_prime_factor<F: FnMut(u128) -> u128>(mut n: u128, f: &mut F) -> u128 {
-    assert!(n >= 2);
-    if n.is_multiple_of(2) {
-        return 2;
-    }
-    while !is_prime(n) {
-        let d = f(n);
-        n = if is_prime(d) {
-            d
-        } else if is_prime(n / d) {
-            n / d
-        } else {
-            d.min(n / d)
-        };
-    }
-    n
-}
+pub trait Factorize {
+    /// `n` の非自明な約数を返す．
+    ///
+    /// `n` は 3 以上の奇数が渡されると仮定して実装して良い．
+    fn find_factor(&mut self, n: u128) -> u128;
 
-/// 素因数分解をする．
-///
-/// `f`: 非自明な約数を返すとする．
-pub fn factorize<F: FnMut(u128) -> u128>(mut n: u128, f: &mut F) -> Vec<u128> {
-    assert_ne!(n, 0);
-    let mut ans = vec![2; n.trailing_zeros() as _];
-    n >>= n.trailing_zeros();
-    while n > 1 {
-        let p = find_prime_factor(n, f);
-        while n.is_multiple_of(p) {
-            n /= p;
-            ans.push(p);
+    /// `n` の素因数を 1 つ返す．
+    ///
+    /// `n >= 2` を仮定する．
+    fn find_prime_factor(&mut self, mut n: u128) -> u128 {
+        assert!(n >= 2);
+        if n.is_multiple_of(2) {
+            return 2;
         }
+        while !is_prime(n) {
+            let d = self.find_factor(n);
+            assert_ne!(d, 1);
+            assert_ne!(d, n);
+            n = if is_prime(d) {
+                d
+            } else if is_prime(n / d) {
+                n / d
+            } else {
+                d.min(n / d)
+            };
+        }
+        n
     }
-    ans.sort();
-    ans
+
+    /// `n` の素因数分解をする．
+    ///
+    /// `n >= 1` を仮定する．
+    fn factorize(&mut self, mut n: u128) -> Vec<u128> {
+        assert!(n >= 1);
+        let mut ans = vec![2; n.trailing_zeros() as _];
+        n >>= n.trailing_zeros();
+        while n > 1 {
+            let p = self.find_prime_factor(n);
+            while n.is_multiple_of(p) {
+                n /= p;
+                ans.push(p);
+            }
+        }
+        ans.sort();
+        ans
+    }
 }
