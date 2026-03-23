efd7c56a2a7a5f621204a8638d72330f79f0809b
wasd314 <68897049+wasd314@users.noreply.github.com>
2026-03-23 10:02:50 +0900

    fix gcd

--- rust/factor/src/utility.rs
+++ rust/factor/src/utility.rs
@@ -1,6 +1,8 @@
+use std::ops::{Bound, RangeBounds};
+
 pub fn gcd(mut x: u128, mut y: u128) -> u128 {
     while x != 0 {
-        (x, y) = (y, y % x);
+        (x, y) = (y % x, x);
     }
     y
 }
