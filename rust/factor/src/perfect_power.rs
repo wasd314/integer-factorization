a7a81eaaf1481f1d0612d4b22e2e2c934bfabd82
wasd314 <68897049+wasd314@users.noreply.github.com>
2026-04-30 14:26:58 +0900

    perfect_power: impl with bisect (not so fast)

--- rust/factor/src/perfect_power.rs
+++ rust/factor/src/perfect_power.rs
@@ -1,5 +1,5 @@
 /// for n > 0, floor(y^{1/n}).
-pub const fn nth_root_floor(y: u128, n: u32) -> u128 {
+pub const fn nth_root_floor1(y: u128, n: u32) -> u128 {
     assert!(n > 0);
     if n == 1 || y <= 1 {
         return y;
@@ -14,7 +14,7 @@ pub const fn nth_root_floor(y: u128, n: u32) -> u128 {
     // y >= 2^n, n >= 3
     let mut x = 0;
     // y < 2^B ==> x < 2^ceil(B/n)
-    let mut i = u128::BITS.div_ceil(n);
+    let mut i = (y.ilog2() + 1).div_ceil(n);
     while i > 0 {
         i -= 1;
         let nx: u128 = x | 1 << i;
@@ -26,6 +26,41 @@ pub const fn nth_root_floor(y: u128, n: u32) -> u128 {
     }
     x
 }
+
+/// for n > 0, floor(y^{1/n}).
+pub const fn nth_root_floor2(y: u128, n: u32) -> u128 {
+    assert!(n > 0);
+    if n == 1 || y <= 1 {
+        return y;
+    }
+    // if n == 2 {
+    //     return y.isqrt();
+    // }
+    // x < 2 <==> y < 2^n, x >= 1.
+    if y.unbounded_shr(n) == 0 {
+        return 1;
+    }
+    // y >= 2^n, n >= 3
+    // y < 2^B ==> x < 2^ceil(B/n)
+    let mut x: u128 = 1 << (y.ilog2() + 1).div_ceil(n);
+    loop {
+        let q = if let Some(xp) = x.checked_pow(n - 1) {
+            y / xp
+        } else {
+            0
+        };
+        if x <= q {
+            break x;
+        }
+        let nx = x - (x - q).div_ceil(n as _);
+        if nx >= x {
+            break x;
+        }
+        x = nx;
+    }
+}
+
+pub use self::nth_root_floor1 as nth_root_floor;
 
 /// `Some((x, n))` s.t. x^n == y, x >= 2, n >= 2, largest n; else `None`.
 pub const fn find_perfect_power(y: u128) -> Option<(u128, u32)> {
@@ -50,13 +85,14 @@ mod tests {
 
     /// check x^n <= y < (x+1)^n.
     fn check_nth_root(y: u128, n: u32) {
-        let x = nth_root_floor(y, n);
+        let x = nth_root_floor1(y, n);
         assert!(
             x.checked_pow(n).is_some_and(|ny| ny <= y),
             "{:?} => {x}, {:?}",
             (y, n),
             x.overflowing_pow(n)
         );
+        assert_eq!(nth_root_floor1(y, n), nth_root_floor2(y, n));
         if x < !0 {
             assert!(
                 (x + 1).checked_pow(n).is_none_or(|ny| ny > y),
@@ -79,7 +115,7 @@ mod tests {
             check_root(!y);
         }
         for n in 1..=u128::BITS {
-            let x0 = nth_root_floor(!0, n);
+            let x0 = nth_root_floor1(!0, n);
             for x in (0..=x0).take(100).chain((0..=x0).rev().take(100)) {
                 let y0 = x.checked_pow(n).unwrap();
                 for i in -5..=5 {
@@ -93,7 +129,7 @@ mod tests {
     #[test]
     fn test_perfect_power() {
         for n in 2..u128::BITS {
-            let x0 = nth_root_floor(!0, n);
+            let x0 = nth_root_floor1(!0, n);
             for x in (2..=x0).take(100).chain((2..=x0).rev().take(100)) {
                 let y0 = x.checked_pow(n).unwrap();
                 assert_ne!(find_perfect_power(y0), None, "{x}^{n}");
